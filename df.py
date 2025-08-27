83c4fcc377173cc02e8622a8d4fd90ad0db51e34
LuBoFu <1453957280@qq.com>
2025-08-27 09:58:57 +0800

    change

--- df.py
+++ df.py
@@ -1,4 +1,6 @@
 from transformers import pipeline
+from huggingface_hub import snapshot_download
+snapshot_download(repo_id="bert-base-uncased", endpoint="https://hf-mirror.com")
 
 generator = pipeline("text-to-image", model="runwayml/stable-diffusion-v1-5", device=0)
 image = generator("a beautiful sunset over the mountains").images[0]
